c43fe8e591535be81a9666c913488b19327c4d2b
krissgpl <kris.sg@wp.pl>
2022-01-22 16:36:43 +0100

    zmina warunku

--- supla_update.sh
+++ supla_update.sh
@@ -124,7 +124,7 @@ echo "$PLIK";
 echo "$PLIK2";
 
 
-if [ -e /media/QNAP/ESP_Firmware/signed/$PLIK && -e /media/QNAP/ESP_Firmware/signed/$PLIK2 ]
+if [ -e /media/QNAP/ESP_Firmware/signed/$PLIK ] && [ -e /media/QNAP/ESP_Firmware/signed/$PLIK2 ]
 then
 
 	dialog --clear --backtitle "Znalazlem $PLIK w QNAP signed Znalazlem $PLIK2 w QNAP signed" --yesno "Czy skopiowac  ?" 10 30
