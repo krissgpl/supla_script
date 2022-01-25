5a3ed4b4d0be4baf01dd6ee72c7a3cc8e05d49a5
Kris_work <krisu.sg@gmail.com>
2022-01-25 14:29:50 +0100

    dopasowanie okna

--- supla_update.sh
+++ supla_update.sh
@@ -179,7 +179,7 @@ cp /media/QNAP/ESP_Firmware/signed/$PLIK2 /var/www/html/update/$PLIK2
 			#grep -n "^${PLIK2}" > wynik.txt;
 			VER=$(cut -f 5 update.txt | tail -1);
 			echo "$VER";
-			NEWVER=$( dialog --title "Dla $BOARD wersja softu : $VER" --inputbox "Wprowadz nowa wersje:" 8 40 3>&1 1>&2 2>&3 3>&- )
+			NEWVER=$( dialog --inputbox "Dla $BOARD wersja softu : $VER   Wprowadz nowa wersje:" 10 40 3>&1 1>&2 2>&3 3>&- )
 			echo "Nowa wersja : $NEWVER";
 		elif [ "$YOUR_CHOOSE" == 1 ];
 		then
