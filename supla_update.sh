37ee6bcb27a79bc43f03fc7b1a72e922e3858eaa
Kris_work <krisu.sg@gmail.com>
2022-01-25 10:05:41 +0100

    eksport wybeanych wpisow

--- supla_update.sh
+++ supla_update.sh
@@ -164,10 +164,11 @@ cp /media/QNAP/ESP_Firmware/signed/$PLIK2 /var/www/html/update/$PLIK2
 		if [ "$YOUR_CHOOSE" == 0 ];
 		then
 			echo "wpis esp_update dla $BOARD";
+			PATH=get_file.php?file=$PLIK;
 			cd /home/pi
-			source supla-docker/.env && docker exec supla-db mysql -u supla --password=$DB_PASSWORD supla -e "SELECT * FROM esp_update" > update.txt;
-			echo "$PLIK2" > fraza.txt;
-			grep -n "^${PLIK2}" > wynik.txt;
+			source supla-docker/.env && docker exec supla-db mysql -u supla --password=$DB_PASSWORD supla -e "SELECT * FROM esp_update WHERE path=$PATH" > update.txt;
+			#echo "$PLIK2" > fraza.txt;
+			#grep -n "^${PLIK2}" > wynik.txt;
 		elif [ "$YOUR_CHOOSE" == 1 ];
 		then
 			echo "Wybrałeś Nie";
