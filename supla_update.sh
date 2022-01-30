e2ce94207ef443c01e58e03901cde1d2cf64174b
krissgpl <kris.sg@wp.pl>
2022-01-30 12:47:25 +0100

    sprawdzenie czy wpisy sie zgadzaja

--- supla_update.sh
+++ supla_update.sh
@@ -10,7 +10,7 @@ TITLE="PLYTKI"
 MENU="Wybierz plytke:"
 
 WYNIK=0;
-WYNIK2=0;
+#WYNIK2=0;
 
 OPTIONS=(1 "k_gate_module_v3"
 		 2 "k_dimmer"
@@ -210,11 +210,14 @@ cp /media/QNAP/ESP_Firmware/signed/$PLIK2 /var/www/html/update/$PLIK2
 					;;
 			esac
 			dialog --backtitle "SUPLA FIRMWARE UPDATE" --title "Wpis w esp_update przed modyfikacja :" --textbox "update.txt" 20 185
+			LINIE=$( wc -l update.txt);
+			echo "Liczba lini w update.txt : $LINIE";
 			while read line; do
-				if echo "$line" | grep -q "$PLIK"; then WYNIK=1; fi
-				if echo "$line" | grep -q "$PLIK2"; then WYNIK2=1; fi
+				if echo "$line" | grep -q "$PLIK"; then ((WYNIK++)); fi
+				if echo "$line" | grep -q "$PLIK2"; then ((WYNIK++)); fi
 			done < update.txt
-			echo "WYNIK=$WYNIK, WYNIK2=$WYNIK2";
+			echo "WYNIK=$WYNIK, LINIE=$LINIE";
+			exit;
 		    while :
 		    do
 				if [ $WYNIK == 1 ] && [ $WYNIK2 == 1 ];
