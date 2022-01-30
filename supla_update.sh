f96165df72eb66232a4f940a97becc5c6ddaa933
krissgpl <kris.sg@wp.pl>
2022-01-30 12:54:03 +0100

    wyswietlenie wyniku porownania

--- supla_update.sh
+++ supla_update.sh
@@ -211,12 +211,18 @@ cp /media/QNAP/ESP_Firmware/signed/$PLIK2 /var/www/html/update/$PLIK2
 			esac
 			dialog --backtitle "SUPLA FIRMWARE UPDATE" --title "Wpis w esp_update przed modyfikacja :" --textbox "update.txt" 20 185
 			LINIE=$( wc -l update.txt);
+			LINIE=LINIE-1;
 			echo "Liczba lini w update.txt : $LINIE";
 			while read line; do
 				if echo "$line" | grep -q "$PLIK"; then ((WYNIK++)); fi
 				if echo "$line" | grep -q "$PLIK2"; then ((WYNIK++)); fi
 			done < update.txt
 			echo "WYNIK=$WYNIK, LINIE=$LINIE";
+			if [ $WYNIK == $LINIE }
+			then
+				echo " Wpisy sie zgadzaja "
+			else " Wpisy sie nie zgadzaja!!! "
+			fi
 			exit;
 		    while :
 		    do
